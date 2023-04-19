d1a8f26983a3b8958061d255ad050a3dfa49459e
Nidhi-Sharma9419 <114356358+Nidhi-Sharma9419@users.noreply.github.com>
2023-04-19 23:32:09 +0530

    Add files via upload

---
+++ IncreasingSubarray.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class IncreasingSubarray {
+    public static void main(String args[]){
+        //int arr[] = new int[]; since size is not known so maybe can't use array this way, instead use list
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        Scanner sc = new Scanner(System.in);
+        arr.add(sc.nextInt());
+
+
+
+    }
+}

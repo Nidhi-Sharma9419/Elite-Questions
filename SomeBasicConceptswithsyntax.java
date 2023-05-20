93679cc5de5e114e444b2107b6f7262bc811a41c
NIDHI SHARMA <114356358+Nidhi-Sharma9419@users.noreply.github.com>
2023-05-20 23:19:39 +0530

    Update SomeBasicConceptswithsyntax.java

--- SomeBasicConceptswithsyntax.java
+++ SomeBasicConceptswithsyntax.java
@@ -1,1 +1,6 @@
 //ITERATOR
+/*A Java Cursor is an Iterator, which is used to iterate or traverse or retrieve a Collection or Stream object’s elements one by one. There are three cursors in Java.
+
+Iterator
+Enumeration
+ListIterator*/
